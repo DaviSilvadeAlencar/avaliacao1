0829a58d8490eb032132a295cf60cda6fd3fd54b
DaviSilvadeAlencar <davimentoriatech@gmail.com>
2025-06-06 10:39:42 -0300

    Update Questao2.java

--- Questao2.java
+++ Questao2.java
@@ -1,9 +1,24 @@
 package avaliacao1;
-
+import java.util.Scanner;
+import java.util.Locale;
 public class Questao2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		System.out.print("Digite a temperatura em C°: ");
+		double c = sc.nextDouble();
+		
+		double f = (c * 9/5) + 32;
+		double k = c + 273.15;
+		
+		System.out.print("Tempertatura em C°: " + c + 
+				"\n Temperatura em °F: " + f + 
+				"\n Temperatura em K: " + k);
+		
+		
 
 	}
 
